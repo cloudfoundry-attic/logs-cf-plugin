c06293a087926d345a548a6a5bf60ca79f110b85
Matthew McNew <mmcnew@pivotallabs.com>
2013-09-03 13:47:08 -0600

    Remove trailing newlines from log messages [#55994892]
    Signed-off-by: Stephan Hagemann <stephan@pivotallabs.com>

--- lib/logs-cf-plugin/message_writer.rb
+++ lib/logs-cf-plugin/message_writer.rb
@@ -4,7 +4,7 @@ module MessageWriter
   include CF::Interactive
   def write(log_target, output, message)
     msg = [log_target.app_name, message.source_type_name, message.source_id, message.message_type_name, message.message].join(" ")
-    msg = c(msg, :error) if message.message_type == LogMessage::MessageType::ERR
+    msg = c(msg.chomp, :error) if message.message_type == LogMessage::MessageType::ERR
     output.puts(msg)
   end
 end
